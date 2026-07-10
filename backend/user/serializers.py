ad4e31827b9ba70c8bc48bfe422c708dbfba9533
MdRaselHosen <raselh280@gmail.com>
2026-07-10 18:22:10 +0600

    create user model and login/register functionalities

---
+++ backend/user/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, att):
+        if att['password'] != att['confirm_password']:
+            raise serializers.ValidationError({'password': "Passwords dosen't match"})
+        
+        return att
+    
+    def create(self, valid_data):
+        valid_data.pop('confirm_password')
+
+        user = User.objects.create_user(**valid_data)
+
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, att):
+        email = att.get('email')
+        password = att.get('password')
+
+        user = authenticate(email=email, password = password)
+
+        if not user:
+            raise serializers.ValidationError("User not found")
+        
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            'user': {
+                "id": user.id,
+                "email": user.email
+            },
+            'access': str(refresh.access_token),
+            'refresh': str(refresh)
+        }
---
+++ backend/user/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            return Response(
+                {
+                    "message": "User created successfully",
+                    "user":{
+                        "id": user.id,
+                        "email": user.email
+                    }
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginView(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(data = request.data)
+
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+
+
